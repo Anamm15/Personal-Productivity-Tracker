68b34c78e33e051178b79e855c0557fa12ceb825
Choirul Anam <choirulanm.15@gmail.com>
2025-12-20 19:30:35 +0700

    migrate to db cloud

---
+++ drizzle/0003_white_mach_iv.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "tasks" ALTER COLUMN "start_time" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "tasks" ALTER COLUMN "end_time" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "tasks" ALTER COLUMN "date" SET NOT NULL;
